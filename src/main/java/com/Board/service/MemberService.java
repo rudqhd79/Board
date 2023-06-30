eed80dffffd1edfb96a59034a4dc77d79747199c
rudqhd79 <rudqhd79@naver.com>
2023-06-30 19:46:22 +0900

    20230630 2

--- src/main/java/com/Board/service/MemberService.java
+++ src/main/java/com/Board/service/MemberService.java
@@ -1,5 +1,9 @@
 package com.Board.service;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.Board.entity.Member;
@@ -11,9 +15,26 @@ import lombok.RequiredArgsConstructor;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
 	private final MemberRepository memberRepository;
+	
+	// Spring Security에 담은 사용자의 정보를 담는 것이 UserDetails이다 (VO 역할)
+	@Override
+	public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+		
+		// userName의 매개변수는 스프링 시큐리티의 로그인 파라메터 값이 memberRepository를 통해 해당 회원을 가져온다
+		Member member = memberRepository.findByLoginId(loginId);
+		
+		// 해당 유저가 없으면 로그인 아이디를 포함한 에러가 발생
+		if (member == null) {
+			throw new UsernameNotFoundException(loginId);
+		}
+		// User객체는 UserDetails 메소드 내에 있는 객체라고 할 수 있다.
+		// User에 관한 정보를 ID Password Role을 이용하여 유저를 만든다
+		return User.builder().username(member.getLoginId()).password(member.getPassword())
+				  .roles(member.getRole().toString()).build();
+	}
 	
 	public Member save (Member member) {
 		validateDuplication(member);
@@ -26,5 +47,6 @@ public class MemberService {
 			throw new IllegalStateException("이미 존재하는 회원입니다.");
 		}
 	}
+
 	
 }
--- src/test/java/com/example/Board/Service/MemberServiceTest.java
+++ src/test/java/com/example/Board/Service/MemberServiceTest.java
@@ -1,22 +1,30 @@
 package com.example.Board.Service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.Board.constant.MemberRole;
 import com.Board.dto.MemberDto;
 import com.Board.entity.Member;
-import com.Board.repository.MemberRepository;
 import com.Board.service.MemberService;
 
 import jakarta.transaction.Transactional;
 
 @SpringBootTest
 @Transactional
-@TestPropertySource(locations = "classpath:application-test.properties")
-public class MemberServiceTest {
+@RunWith(SpringRunner.class)
+@Import(MemberService.class)
+class MemberServiceTest {
 	
 	@Autowired
 	MemberService memberService;
@@ -24,9 +32,34 @@ public class MemberServiceTest {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	
+	@Test
 	Member createMember() {
 		MemberDto dto = new MemberDto();
+		dto.setEmail("test@email.com");
+		dto.setName("김테스트");
+		dto.setNickName("테스터킴");
+		dto.setLoginId("test");
+		dto.setPhone("010-0000-0000");
+		dto.setPassword("test1234");
 		return Member.createMember(dto, passwordEncoder, MemberRole.USER);
+	}
+	
+	@Test
+	@Transactional
+	void saveMember() {
+		// given
+		Member member = createMember();
+		// when
+		Member saveMember = memberService.save(member);
+		// then
+		assertEquals(member.getEmail(), saveMember.getEmail());
+		assertEquals(member.getName(), saveMember.getName());
+		assertEquals(member.getNickName(), saveMember.getNickName());
+		assertEquals(member.getLoginId(), saveMember.getLoginId());
+		assertEquals(member.getPhone(), saveMember.getPhone());
+		assertEquals(member.getPassword(), saveMember.getPassword());
+		assertEquals(member.getRole(), saveMember.getRole());
+		fail("정보가 같지 않습니다.");
 	}
 
 }
