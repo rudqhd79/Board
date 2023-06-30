556f26577aea80887611df3f6cfbe58e336593e1
rudqhd79 <rudqhd79@naver.com>
2023-06-30 23:21:58 +0900

    20230630 3

--- src/main/java/com/Board/config/AuditConfig.java
+++ src/main/java/com/Board/config/AuditConfig.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AuditConfig {
 	
 	// 이 메소드는 로그인한 사용자를 감지하기 위한 메소드이다. (자동 센서)
-	@Bean
+	@Bean // 싱글톤이 보장되지만 @Configuration이 적용된 범위 내에서만 적용된다
 	public AuditorAware<String> auditorProvider() {
 		return new AuditorAwareImpl();
 	}
